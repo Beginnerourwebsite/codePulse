15647ee4380acd89fce37be7d200fbdcc25f037e
pankaj <pankajdesktop23@gmail.com>
2024-07-30 11:54:04 +0530

    update codes

--- assets/js/self/Nav.js
+++ assets/js/self/Nav.js
@@ -1,102 +1,96 @@
-const apiUrl = './assets/js/self/Api.json';
+const apiUrl = "./assets/js/self/Api.json";
 function ApiurlFetch(match = "") {
-    return new Promise(function (resolve, reject) {
-        fetch(apiUrl)
-            .then(function (response) {
-                return response.json();
-            })
-            .then(function (data) {
-                // console.clear();
-                createNavbar(data);
-                if (match != "") {
-
-                    resolve(data.findIndex(x => x.url.split("./")[1] == match.split("/")[1]))
-                }
-
-            })
-            .catch(function (error) {
-                console.error('Error fetching data:', error);
-            });
-    })
-}
-ApiurlFetch()
-function createNavbar(apiData) {
-
-
-
-    let navbarHTML = '';
-    apiData.forEach((menuItem, index) => {
-        if (menuItem.submenu) {
-
-            navbarHTML += `<li class="has-sub"> <a href="${menuItem.url}">${menuItem.icon}<span class="title">${menuItem.label}</span></a>
-            <ul >`;
-            menuItem.submenu.forEach(subItem => {
-
-                navbarHTML += `<li> <a href="${subItem.subUrl}">${subItem.icon}<span
-                class="title">${subItem.subname}</span></a> </li>`;
-            });
-            navbarHTML += `</ul></li>`;
-        } else {
-            navbarHTML += `<li id="${index}" onclick="activateOrNot(this)" > <a href="${menuItem.url}" >${menuItem.icon}<span
-            class="title" >${menuItem.label}</span></a> </li>`;
+  return new Promise(function (resolve, reject) {
+    fetch(apiUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        // console.clear();
+        createNavbar(data);
+        if (match != "") {
+          resolve(
+            data.findIndex((x) => x.url.split("./")[1] == match.split("/")[1])
+          );
         }
-    });
-    document.getElementById('main-menu').innerHTML = navbarHTML;
-
+      })
+      .catch(function (error) {
+        console.error("Error fetching data:", error);
+      });
+  });
+}
+ApiurlFetch();
+function createNavbar(apiData) {
+  let navbarHTML = "";
+  apiData.forEach((menuItem, index) => {
+    if (menuItem.submenu) {
+      navbarHTML += `<li class="has-sub"> <a href="${menuItem.url}">${menuItem.icon}<span class="title">${menuItem.label}</span></a>
+            <ul >`;
+      menuItem.submenu.forEach((subItem) => {
+        navbarHTML += `<li> <a href="${subItem.subUrl}">${subItem.icon}<span
+                class="title">${subItem.subname}</span></a> </li>`;
+      });
+      navbarHTML += `</ul></li>`;
+    } else {
+      navbarHTML += `<li id="${index}" onclick="activateOrNot(this)" > <a href="${menuItem.url}" >${menuItem.icon}<span
+            class="title" >${menuItem.label}</span></a> </li>`;
+    }
+  });
+  document.getElementById("main-menu").innerHTML = navbarHTML;
 }
 
 function activateOrNot(a) {
-    
-    sessionStorage.setItem('active', (a.id));
-
+  sessionStorage.setItem("active", a.id);
 }
 
 window.addEventListener("load", async function () {
-    console.log(window.location.pathname)
-    let index = await ApiurlFetch(window.location.pathname)
-    console.log(index)
-    sessionStorage.setItem('active', index)
+  let index = await ApiurlFetch(window.location.pathname);
 
-    // console.log("first")
-    setTimeout(async () => {
-        let sessionStoragedata = sessionStorage.getItem('active')
-        if (sessionStoragedata == null) {
-            document.getElementById(sessionStorage.getItem('active')).setAttribute("class", 'active');
+  sessionStorage.setItem("active", index);
 
-        }
-        else {
-            document.getElementById(sessionStorage.getItem('active')).setAttribute("class", 'active');
-        }
-    }, 1000)
-    // document.getElementById(sessionStorage.getItem('active')).setAttribute("class", 'active');
-})
+  // console.log("first")
+  setTimeout(async () => {
+    let sessionStoragedata = sessionStorage.getItem("active");
+    if (sessionStoragedata == null) {
+      document
+        .getElementById(sessionStorage.getItem("active"))
+        .setAttribute("class", "active");
+    } else {
+      if (document.getElementById(sessionStorage.getItem("active")) != null)
+        document
+          .getElementById(sessionStorage.getItem("active"))
+          .setAttribute("class", "active");
+    }
+  }, 1000);
+  // document.getElementById(sessionStorage.getItem('active')).setAttribute("class", 'active');
+});
 
 // navbar code end
 
-
 function Addclass(self, className) {
-    self.classList.add(className);
+  self.classList.add(className);
 }
 function removeclass(self, className) {
-    self.classList.remove(className);
+  self.classList.remove(className);
 }
 
-let titletag = document.querySelector("title")
+let titletag = document.querySelector("title");
 if (titletag == null) {
-    let createTitleTag = document.createElement("title")
-    createTitleTag.innerHTML = "Code Pulse"
+  let createTitleTag = document.createElement("title");
+  createTitleTag.innerHTML = "Code Pulse";
+} else {
+  titletag.innerHTML = "Code Pulse";
 }
-else {
-    titletag.innerHTML = "Code Pulse"
-}
-let logoLink = document.querySelector("link[rel='icon'")
+let logoLink = document.querySelector("link[rel='icon'");
 
 if (logoLink == null) {
-    let createLogoLink = document.createElement("link")
-    createLogoLink.rel = "icon"
-    createLogoLink.href = "./assets/images/logo-removebg-preview.png"
+  let createLogoLink = document.createElement("link");
+  createLogoLink.rel = "icon";
+  createLogoLink.href = "./assets/images/logo-removebg-preview.png";
+} else {
+  logoLink.href = "./assets/images/logo-removebg-preview.png";
 }
-else {
-    logoLink.href = "./assets/images/logo-removebg-preview.png"
+{
+  /* <link rel="icon" href="./assets/images/logo.png"> */
 }
-{/* <link rel="icon" href="./assets/images/logo.png"> */ }
+let firetypeuser = false;
--- assets/js/self/firebase.js
+++ assets/js/self/firebase.js
@@ -1,62 +1,84 @@
-{/* <script src="https://www.gstatic.com/firebasejs/10.10.0/firebase-app-compat.js"></script>
-<script src="https://www.gstatic.com/firebasejs/10.10.0/firebase-database-compat.js"></script>
-<script src="./assets/js/self/firebase.js"></script> */}
-
-function firebaseDatabase(root = "", datas = {}) {
-    return new Promise(function (resolve, reject) {
-        const firebaseConfig = {
-            apiKey: "AIzaSyCnxjFIpXvWQjyEa6jPO08-Lt4rBu03ews",
-            authDomain: "mystorage-9621a.firebaseapp.com",
-            databaseURL: "https://mystorage-9621a-default-rtdb.firebaseio.com",
-            projectId: "mystorage-9621a",
-            storageBucket: "mystorage-9621a.appspot.com",
-            messagingSenderId: "962491672083",
-            appId: "1:962491672083:web:e86539d51606f2c3db81e6"
-        };
-        const firebaseApp = firebase.initializeApp(firebaseConfig);
-        let data = firebaseApp.database().ref()
-        console.log(data)
-        if (typeof (datas) == "object") {
-
-            data.child(root).push(datas, (err) => {
-                if (!err) {
-                    resolve(true)
-                }
-            })
-        }
-        else {
-            data.child(root).on("value", (snap) => {
-                
-                if (snap.val()) {
-
-                    let arr = []
-                    Object.values(snap.val()).forEach(element => {
-                        arr.push(element)
-                    })
-                    resolve(arr)
-                }
-                else{
-                  
-                    reject("error")
-                }
-            })
-        }
-    })
+//<script src="https://www.gstatic.com/firebasejs/10.10.0/firebase-app-compat.js"></script>
+//<script src="https://www.gstatic.com/firebasejs/10.10.0/firebase-database-compat.js"></script>
+//<script src="./assets/js/self/firebase.js"></script>
+function fireCon() {
+  const firebaseConfig = {
+    apiKey: "AIzaSyDO7oTnAJfrT5MuYSYr2bfyQcOEOtqlZrY",
+    authDomain: "codepulse-55f74.firebaseapp.com",
+    projectId: "codepulse-55f74",
+    storageBucket: "codepulse-55f74.appspot.com",
+    messagingSenderId: "210958102368",
+    appId: "1:210958102368:web:06baa3c25ee70a6846f805",
+  };
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  return firebaseApp.database().ref();
 }
-
+function firebaseDatabase(root = "", datas = {}) {
+  return new Promise(function (resolve, reject) {
+    let data = fireCon();
+    if (typeof datas == "object" && Object.keys(datas).length > 0) {
+      data.child(root).push(datas, (err) => {
+        if (!err) {
+          resolve(true);
+        }
+      });
+    } else {
+      data.child(root).on("value", (snap) => {
+        if (snap.val() != null) {
+          console.log(snap.val());
+          let arr = [];
+          Object.keys(snap.val()).forEach((element) => {
+            arr.push({ ...snap.val()[element], keys: element });
+          });
+          resolve(arr);
+        } else {
+          reject(snap.val());
+        }
+      });
+    }
+  });
+}
 
 function timeanddate() {
-    let month = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"]
-    let week = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
-    let timeStatus = "PM"
-    if (JSON.stringify(new Date().getHours()).length == 1) {
-        timeStatus = 'AM'
-    }
-    let dateObj = {
-        date: `${new Date().getDate()} ${month[[new Date().getMonth()]]},${new Date().getFullYear()}`,
-        time: `${JSON.stringify(new Date().getHours()).padStart(2, 0)}:${new Date().getMinutes()} ${timeStatus}(${week[new Date().getDay()]})`,
-        year: new Date().getFullYear()
-    }
+  let month = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sept",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  let week = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let timeStatus = "PM";
+  if (JSON.stringify(new Date().getHours()).length == 1) {
+    timeStatus = "AM";
+  }
+  let dateObj = {
+    date: `${new Date().getDate()} ${
+      month[[new Date().getMonth()]]
+    },${new Date().getFullYear()}`,
+    time: `${JSON.stringify(new Date().getHours()).padStart(
+      2,
+      0
+    )}:${new Date().getMinutes()} ${timeStatus}(${week[new Date().getDay()]})`,
+    year: new Date().getFullYear(),
+  };
 
-    return dateObj;
+  return dateObj;
 }
+
+window.addEventListener("keyup", function (e) {
+  if (e.altKey) {
+    if (e.key == "p") {
+      firetypeuser = true;
+    } else if (e.key == "m" && firetypeuser) {
+      window.open("/Admin.html");
+    }
+  }
+});
